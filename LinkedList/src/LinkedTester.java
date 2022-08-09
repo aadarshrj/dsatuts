337e1f906d71e28d572f002bab7710f0e38206fb
Adarsh Raj <aadarshrj007@gmail.com>
2022-08-09 23:52:04 +0530

    second changes in git

--- LinkedList/src/LinkedTester.java
+++
@@ -1,9 +0,0 @@
-public class LinkedTester {
-    public static void main(String[] args) {
-        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
-        list.addHead(5);
-        list.addTail(8);
-        list.add(1,24);
-        list.printList();
-    }
-}
